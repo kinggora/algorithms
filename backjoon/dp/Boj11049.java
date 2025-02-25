03d19a2a3177b3b671492fa8efeb2e3f4c91851c
kinggora <jhha09@ajou.ac.kr>
2025-02-26 00:02:04 +0900

    백준 11049: 행렬 곱셈 순서

--- backjoon/dp/Boj11049.java
+++ backjoon/dp/Boj11049.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Boj11049 {
 
     static int[][] cnt;
     static int[][] matrix;
